126d077030605fd65aab83ca723a0512ef58f6a9
Peter Andreas Moelgaard <gitkraken@petermolgaard.com>
2022-04-29 18:08:58 +0700

    Avenida - Frontend Developer - Greater Copenhagen

--- print_to_pdf.sh
+++ print_to_pdf.sh
@@ -6,7 +6,6 @@ echo ######################################################################
 echo "CONVERTING:";
 
 echo "Target: index.html";
-echo "Destination: Peter Andreas Moelgaard CV 2021";
+echo "Destination: Peter Andreas Moelgaard CV 2022";
 
-pagedjs-cli "index-en.html" -o "Peter Andreas Moelgaard CV 2022 - EN.pdf";
-pagedjs-cli "index-dk.html" -o "Peter Andreas Moelgaard CV 2022 - DK.pdf";
+pagedjs-cli "index.html" -o "Peter Andreas Moelgaard CV 2022.pdf";
